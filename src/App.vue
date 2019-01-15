a29fa4fef061d04a9d497deedc716aefcacdd1f5
ranranqi <1137625794@qq.com>
2019-01-15 18:42:37 +0800

    添加头部导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    
+    <Header/>
   </div>
 </template>
 
 <script>
+ import Header from './components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header
+  }
 }
 </script>
 
 <style>
-#app {
-
+*{
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,20 @@
 <template>
-  
-      
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="#">
+    <ul>
+      <li><a href="">首页</a></li>
+      <li><a href="">新手入门</a></li>
+      <li><a href="">API</a></li>
+      <li><a href="">关于</a></li>
+      <li><a href="">注册</a></li>
+      <li><a href="">登录</a></li>
+    </ul>
+  </div>  
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'Header',
   data () {
     return {
       
@@ -16,5 +25,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.header{
+  background-color: #444;
+  height: 50px;
+}
+img{
+  max-width: 120px;
+  margin-left: 70px;
+  margin-top: 10px;
+}
+ul{
+  list-style: none;
+  float: right;
+  margin-right: 92px;
+}
+li{
+  display: inline-block;
+  padding: 12px 12px;
+}
+a{
+  text-decoration: none;
+  color: #ccc;
+  font-size: 13px;
+}
 </style>
