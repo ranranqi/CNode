751078a6c8edde2289fbe9f26654a7214776c32d
ranranqi <1137625794@qq.com>
2019-01-17 00:24:42 +0800

    添加标题/事件/和一点css样式

--- src/components/Postlist.vue
+++ src/components/Postlist.vue
@@ -9,12 +9,21 @@
                     <img :src="post.author.avatar_url" alt="">
                     <span>
                         {{post.reply_count}}
-                    </span>/{{post.visit_count}}
+                    </span>/<span>
+                        {{post.visit_count}}
+                    </span>
+                    <span>
+                        {{post.title}}
+                    </span>
+                    <span class="time">
+                        {{post.last_reply_at}}
+                    </span>
                 </li>
             </ul>
         </div>
     </div>
 </template>
+
 <script>
 export default {
     name: 'Postlist',
@@ -26,12 +35,16 @@ export default {
     methods: {
         getData(){
             this.$http.get('https://cnodejs.org/api/v1/topics',{
-                page: 1,
-                limit: 10
+                params:{
+                    page: 1,
+                    limit: 20
+                }
             })
             .then((response)=>{
                 this.posts = response.data.data
-                console.log(response.data.data)
+            })
+            .catch((error)=>{
+                console.log(error)
             })
         }
     },
@@ -40,11 +53,28 @@ export default {
     }
 }
 </script>
+
 <style scoped>
 .postlist img{
     width: 30px;
     height: 30px;
     vertical-align: middle;
+}
+ul{
+    list-style: none;
+    background: #fff;
+    background-color: #f6f6f6;
+}
+.postlist ul li{
+    padding: 10px;
+    background: #fff;
+    border-bottom: 1px solid #f6f6f6;
+    border-radius: 3px 3px 0 0;
+    width: 88%;
+    margin: 0 auto;
+}
+.postlist ul li .time{
+    text-align: right;
 }
 </style>
 
